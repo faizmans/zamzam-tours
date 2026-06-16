762f551ddc28efb2b6e90b9a2f1a629f6f056e38
faizmansuri <faaiz4095@gmail.com>
2026-06-16 06:08:20 +0000

    demo ready 3

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Building the project..."
-python3 -m pip install -r requirements.txt
+# Use uv instead of pip
+uv pip install -r requirements.txt --system
+
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
